fc99c3ccbea627892e888c89b8c81a70b6b30189
RachelNS <rachelnelsonschille@gmail.com>
2021-01-24 11:25:35 -0800

    created and seeded burger db

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Veggie Burger');
+INSERT INTO burgers (name, devoured) VALUES ('Green Chile Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('BBQ Bacon Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Beyond Burger', true);
+INSERT INTO burgers (name) VALUES ('Blue Cheese Burger');
