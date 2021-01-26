03c793b7628b637c14c1008961d01b8169e1050b
RachelNS <rachelnelsonschille@gmail.com>
2021-01-25 21:59:12 -0800

    added start script to package.json

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,20 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
 
-if(process.env.JAWSBD_URL) {
-  connection = mysql.createConnection(process.envJAWSBD_URL);
-}
-else {
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "burger_db"
-})};
+if (process.env.JAWSBD_URL) {
+  let connection = mysql.createConnection(process.env.JAWSBD_URL);
+}else {
+  let connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "burger_db"
+  })
+};
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
